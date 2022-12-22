8c5d3ccf20444fbc7db8652f4542abe1ca56e32a
Gifar Al Anshar <gifar.anshar12@gmail.com>
2022-12-23 05:38:53 +0700

    generate placement model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@
 
 class Product < ApplicationRecord
   belongs_to :user
+  has_many :placements, dependent: :destroy
+  has_many :orders, through: :placements
 
   validates :name, presence: true, length: { maximum: 100, minimum: 3 }
   validates :price, presence: true,  numericality: { greater_than_or_equal_to: 0 }
