8535b2c7d9e11291dde5a4d530f00332e6b72e4f
Gifar Al Anshar <gifar.anshar12@gmail.com>
2022-12-23 14:38:04 +0700

    remove quantity from products to placements

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -14,4 +14,15 @@ RSpec.describe Order, type: :model do
 
     expect(@product1.price + @product2.price).to eq(order.total)
   end
+
+  it "should build 2 placements for the order" do
+
+    expect do
+      @order.build_placements_with_product_ids_and_quantities([
+        {product_id: @product1, quantity: 2},
+        {product_id: @product2, quantity: 3}
+      ])
+      @order.save
+    end.to change(Order, :count).by(2)
+  end
 end
