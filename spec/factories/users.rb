5ac3a16ec5b6d6bc3bde5e83a85ec5e2ed17e45e
Gifar Al Anshar <gifar.anshar12@gmail.com>
2022-12-24 14:24:39 +0700

    refactor user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,29 +1,28 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
+
+  # this factory for create list of users
   factory :user do
     sequence(:email) { |n| "email#{n}@.com" }
     sequence(:username) { |n| "username#{n}" }
     password { 'Ini-123' }
-    password_digest { BCrypt::Password.create('Ini-2000-Password').to_s }
-    first_name { 'Gifar' }
-    last_name { 'Al Anshar' }
+    password_digest { BCrypt::Password.create('Ini-123').to_s }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+  end
+
+  factory :user1, parent: :user do
+    email { 'user1@ex.com'}
+    username { 'user1'}
+    password { 'Ini-123' }
+    password_digest { BCrypt::Password.create('Ini-123').to_s }
   end
 
   factory :user2, parent: :user do
-    email { 'examp@ex.com'}
-    username { 'example' }
+    email { 'user2@ex.com'}
+    username { 'user2' }
     password { 'Ini-123' }
     password_digest { BCrypt::Password.create('Ini-123').to_s }
-    first_name { 'Nelson' }
-    last_name { 'Mandela' }
-  end
-
-  factory :invalid_user, parent: :user do
-    email { nil }
-    username { nil }
-    password_digest { BCrypt::Password.create('Ini-2000-Password').to_s }
-    first_name { 'Gifar' }
-    last_name { 'Al Anshar' }
   end
 end
